63f3e97322656aba3f8b7fa0df3b26f4c35f360a
TheRiseOfDavid <david53133@gmail.com>
2021-03-05 09:31:28 +0800

    20210305.0931

---
+++ HW01-2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 12:53:57 2021
+
+@author: User
+"""
+
+import cv2
+import numpy as np 
+
+img = np.zeros((400, 400, 3), np.uint8)
+img.fill(200)
+
+cv2.line(img, (16, 16), (100, 100), (240, 169, 169), 8)
+cv2.rectangle(img, (200, 200), (232, 232), (154, 224, 253), -1)
+cv2.rectangle(img, (100, 100), (132, 132), (194, 109, 165), 8)
+cv2.circle(img, (300, 100), 80, (118, 122, 27), 3)
+cv2.circle(img, (100, 300), 16, (135, 133, 255), -1)
+
+cv2.imshow("hello world", img)
+
---
+++ HW01.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 11:57:34 2021
+
+@author: User
+"""
+
+import cv2
+import numpy as np 
+
+img_old = cv2.imread("hw01.jpg")
+w = int(img_old.shape[1] * 0.5)
+h = int(img_old.shape[0] * 0.5)
+
+def re(img):
+
+    
+    # resize 
+    # (x, y) is tuple
+    img2 = cv2.resize(img, (w, h), \
+                      interpolation = cv2.INTER_CUBIC )
+    cv2.imshow("donburi", img2) 
+    return img2
+
+def gray(img):
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
+    cv2.imshow("donburi_gray", img2)
+    return img2 
+
+def HSV(img):
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cv2.imshow("donburi_HSV", img2)
+    return img2 
+
+def YcrCb(img):
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    cv2.imshow("donburi_YcrCb", img2)
+    return img2 
+
+def RGBpass(img):
+    cname = ["B", "G", "R"]
+    for i in range(2,-1,-1):
+        img2 = img[:, :, i]
+        #cv2.imshow("pass" + cname[i], img2)
+        
+        img2 = np.zeros((h, w, 3), np.uint8)
+        img2[:, :, i] = img[:, :, i]
+        cv2.imshow("look" + cname[i], img2)
+
+
+#gray
+img = re(img_old)
+gray(img)
+RGBpass(img)
+
+#hsv
+img = re(img_old)
+img = HSV(img)
+
+#YcrCb
+img = re(img_old)
+img = YcrCb(img)
+
+cv2.waitKey(0)
+cv2.destroyWindows()
+
