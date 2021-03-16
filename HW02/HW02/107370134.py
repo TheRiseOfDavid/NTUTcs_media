2113d1e7c31f4a98ecf5ff5dee54e44d0b01010d
tzulin <tzulin0517@gmail.com>
2021-03-16 17:12:48 +0800

    upload 小組報告zip檔

--- HW02/HW02/107370134.py
+++ HW02/HW02/107370134.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 img1 = cv2.imread('circle.jpg',0)
 
@@ -8,7 +7,6 @@ ret, circle1 = cv2.threshold(img1 ,127,255,cv2.THRESH_OTSU)
 circle2 = cv2.dilate(circle1,np.ones((3,3)),iterations = 7)
 circle3 = cv2.erode(circle2,np.ones((3,3)),iterations = 7)
 cv2.imshow("circle",circle3)
-cv2.waitKey(0)
 
 img2 = cv2.imread('man.jpg',0)
 ret, man1 = cv2.threshold(img2 ,127,255,cv2.THRESH_BINARY_INV)
