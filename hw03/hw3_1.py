527712acd40c73125034571a836f192076ed451e
tzulin <tzulin0517@gmail.com>
2021-03-23 16:59:32 +0800

    add hw3_1.py--題目一完成

---
+++ hw03/hw3_1.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+# hw3_1: 框硬幣
+img_coin = cv2.imread("./pic/coin.jpg")
+w = 800
+h = 450
+kernel = np.ones((2,2), np.uint8)
+
+# 調整大小
+resize_c = cv2.resize(img_coin, (w, h), \
+                      interpolation = cv2.INTER_CUBIC )
+# 灰階
+gray_c = cv2.cvtColor(resize_c, cv2.COLOR_BGR2GRAY)
+# 二值化
+ret, thres_c = cv2.threshold(gray_c, 90, 255, cv2.THRESH_BINARY)
+# 侵蝕
+erode_c = cv2.erode(thres_c, kernel, iterations=2)
+# 膨脹
+dilation_c = cv2.dilate(erode_c, kernel, iterations=1)
+# 連通物件 stats[x, y, h, w, area]
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(dilation_c, connectivity=8)
+count = 0
+for i in range(num_labels):
+    if 60 < stats[i][2] <= 80:  # one dollar
+        cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 0, 255), 2)
+        print('stats = ',stats[i])
+        print('centroids = ',centroids[i])
+        count += 1 
+    if 80 < stats[i][2] <= 90:  # five dollar
+        cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 128, 255), 2)
+        print('stats = ',stats[i])
+        print('centroids = ',centroids[i])
+        count += 1
+    if 90 < stats[i][2] <= 100:  # ten dollar
+        cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 255, 255), 2)
+        print('stats = ',stats[i])
+        print('centroids = ',centroids[i])
+        count += 1
+    if 100 < stats[i][2] <= 110:  # fifty dollar
+        cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 255, 0), 2)
+        print('stats = ',stats[i])
+        print('centroids = ',centroids[i])
+        count += 1
+
+# print("count = ", count)        
+# cv2.imshow("erode", erode_c)
+# cv2.imshow("dilation", dilation_c)
+cv2.imshow("result", resize_c)
+cv2.imwrite("hw3_1.jpg", resize_c)
+
+cv2.waitKey(0)
+cv2.destroyWindow()
