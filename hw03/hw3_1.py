885a11941131b1912176890c19c2b500cbb3ceeb
tzulin <tzulin0517@gmail.com>
2021-03-25 12:25:25 +0800

    add hw3_2

--- hw03/hw3_1.py
+++ hw03/hw3_1.py
@@ -1,3 +1,4 @@
+
 import cv2
 import numpy as np
 
@@ -21,33 +22,42 @@ dilation_c = cv2.dilate(erode_c, kernel, iterations=1)
 # 連通物件 stats[x, y, h, w, area]
 num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(dilation_c, connectivity=8)
 count = 0
+sum = 0 # 金額總和初值
+# 依照框的長來辨別硬幣，並用rectangle框起來
 for i in range(num_labels):
     if 60 < stats[i][2] <= 80:  # one dollar
         cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 0, 255), 2)
-        print('stats = ',stats[i])
+        print('1: stats = ',stats[i])
         print('centroids = ',centroids[i])
         count += 1 
+        sum += 1
     if 80 < stats[i][2] <= 90:  # five dollar
         cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 128, 255), 2)
-        print('stats = ',stats[i])
+        print('5: stats = ',stats[i])
         print('centroids = ',centroids[i])
         count += 1
+        sum += 5
     if 90 < stats[i][2] <= 100:  # ten dollar
         cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 255, 255), 2)
-        print('stats = ',stats[i])
+        print('10: stats = ',stats[i])
         print('centroids = ',centroids[i])
         count += 1
+        sum += 10
     if 100 < stats[i][2] <= 110:  # fifty dollar
         cv2.rectangle(resize_c, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), (0, 255, 0), 2)
-        print('stats = ',stats[i])
+        print('50: stats = ',stats[i])
         print('centroids = ',centroids[i])
         count += 1
+        sum += 50
 
 # print("count = ", count)        
 # cv2.imshow("erode", erode_c)
 # cv2.imshow("dilation", dilation_c)
+print("sum = ", sum)
 cv2.imshow("result", resize_c)
 cv2.imwrite("hw3_1.jpg", resize_c)
 
 cv2.waitKey(0)
 cv2.destroyWindow()
+
+# 困難：測試二值化、膨脹和侵蝕等的數值花了些時間，還有判斷不同硬幣的大小也是透過print出stats的數值微小差異才成功
---
+++ hw03/hw3_2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 10:07:19 2021
+
+@author: kuotzulin
+"""
+
+import cv2
+import numpy as np
+
+# hw3_2: 利用霍夫線標磁磚邊緣
+img_floor = cv2.imread("./pic/floor.jpg")
+w = int(img_floor.shape[1]*0.2)
+h = int(img_floor.shape[0]*0.2)
+kernel = np.ones((2,2), np.uint8)
+
+# 調整大小
+resize_f = cv2.resize(img_floor, (w, h), \
+                      interpolation = cv2.INTER_CUBIC )
+# 灰階
+gray_f = cv2.cvtColor(resize_f, cv2.COLOR_BGR2GRAY)
+# 二值化
+ret, thres_f = cv2.threshold(gray_f, 90, 255, cv2.THRESH_BINARY_INV)
+# 侵蝕
+erode_f = cv2.erode(thres_f, kernel, iterations=1)
+# 膨脹
+dilation_f = cv2.dilate(erode_f, kernel, iterations=1)
+
+# 霍夫直線偵測
+rho = 1
+theta = np.pi/2
+lines = cv2.HoughLines(dilation_f, rho, theta, 200)
+# 畫出直線
+for i in range(len(lines)):
+    for rho,theta in lines[i]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        cv2.line(resize_f, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        
+# cv2.imshow("thres_f", thres_f)
+# cv2.imshow("dilate_f", dilation_f)
+# cv2.imshow("dilation_f", dilation_f)
+cv2.imshow("result", resize_f)
+cv2.imwrite("hw3_2.jpg", resize_f)
+
+cv2.waitKey(0)
+cv2.destroyWindow()
+
+# 困難：一開始不知道 hough line是偵測白色的部分，所以畫出來的線很多又雜。
+# 嘗試很久才發現要改為黑白相反，後來把二值化改為BINARY_INV就可以了。
+# HoughLines是存(r,θ)值，不太確定如何轉換為直角坐標，就上網查了資料，找到上面的解法。
