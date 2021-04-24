b72336384de3a5c5d91b285389b133f8917afb15
tzulin <tzulin0517@gmail.com>
2021-04-24 15:11:39 +0800

    0424 mid project

--- midterm/rename.py
+++ midterm/rename.py
@@ -11,18 +11,18 @@ import cv2
 def dog():
     for i in range(1,101):
         print(i)
-        image = cv2.imread("../dog/images (%d).jfif" % i)
+        image = cv2.imread("./dog/images (%d).jfif" % i)
         image = cv2.resize(image,(100,100))
         
-        cv2.imwrite("../resize_dog/images_%d.jpg" % i, image)    
+        cv2.imwrite("./data/images_%d.jpg" % i, image)    
 
 def cat():
     for i in range(101,201):
         print(i)
-        image = cv2.imread("../cat/images (%d).jfif" % (i-100))
+        image = cv2.imread("./cat/images (%d).jfif" % (i-100))
         image = cv2.resize(image,(100,100))
         
-        cv2.imwrite("../resize_cat/images_%d.jpg" % i, image)
+        cv2.imwrite("./data/images_%d.jpg" % i, image)
 
 dog()
 cat()
